b85421079eee30803ead365bf9f72e58f8369aec
ldu1020 <ldu1020@gmail.com>
2024-06-20 16:44:56 +0900

    fix(gen-api): 🐛fix react query type
    '<' 와 같은 꺽쇠가 escape 처리 되는 이슈를 수정했습니다.

---
+++ .changeset/short-mangos-heal.md
@@ -0,0 +1,7 @@
+---
+'@toktokhan-dev/cli-plugin-gen-api-react-query': patch
+---
+
+fix react query type
+
+'<' 와 같은 꺽쇠가 escape 처리 되는 이슈를 수정했습니다.
--- packages/cli-plugins/gen-api-react-query/src/playground.ts
+++ packages/cli-plugins/gen-api-react-query/src/playground.ts
@@ -1,1 +1,16 @@
-console.log('Hi')
+import { genApi } from '.'
+
+genApi.run({
+  swaggerSchemaUrl: 'https://sales-api-dev.pluuug.com/openapi.json/',
+  output: 'src/generated/apis',
+  includeReactQuery: true,
+  includeReactInfiniteQuery: true,
+  httpClientType: 'axios',
+  instancePath: '@apis/_axios/instance',
+  paginations: [
+    {
+      keywords: ['cursor'],
+      nextKey: 'cursor',
+    },
+  ],
+})
