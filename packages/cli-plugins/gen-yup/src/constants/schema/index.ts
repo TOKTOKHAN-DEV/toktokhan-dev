7240053b7013e04d768ff05358352d3285676e1c
SEOJIWOO_TOKTOKHAN_DEV <a20304645@gmail.com>
2024-10-29 11:51:00 +0900

    [FIX] 오타 수정 & 유선 번호, 회사 번호 추가

--- packages/cli-plugins/gen-yup/src/constants/schema/index.ts
+++ packages/cli-plugins/gen-yup/src/constants/schema/index.ts
@@ -1,5 +1,7 @@
 type Profile =
   | 'phone'
+  | 'cellPhone'
+  | 'businessPhone'
   | 'birthdate'
   | 'nickname'
   | 'username'
@@ -14,7 +16,7 @@ type Authentication =
   | 'newPassword'
   | 'newPasswordConfirm'
 
-type VerificationCode = 'emailVerificationCode' | 'phoneVerificationCode'
+type VerificationCode = 'emailVerificationCode' | 'cellPhoneVerificationCode'
 
 type Address = 'postcode' | 'addressMain' | 'addressDetail' | 'city' | 'region'
 
@@ -27,12 +29,14 @@ export const schemaList: {
   { name: '아이디', value: 'id' },
   { name: '비밀번호', value: 'password' },
   { name: '비밀번호_확인', value: 'passwordConfirm' },
-  { name: '새 비밀번호', value: 'newPassword' },
-  { name: '새 비밀번호_확인', value: 'newPasswordConfirm' },
+  { name: '새_비밀번호', value: 'newPassword' },
+  { name: '새_비밀번호_확인', value: 'newPasswordConfirm' },
   { name: '이메일', value: 'email' },
   { name: '이메일_인증번호', value: 'emailVerificationCode' },
-  { name: '핸드폰_번호', value: 'phone' },
-  { name: '핸드폰_인증번호', value: 'phoneVerificationCode' },
+  { name: '휴대폰_번호', value: 'cellPhone' },
+  { name: '휴대폰_인증번호', value: 'cellPhoneVerificationCode' },
+  { name: '유선_번호', value: 'phone' },
+  { name: '회사_번호', value: 'businessPhone' },
   { name: '생년월일', value: 'birthdate' },
   { name: '닉네임', value: 'nickname' },
   { name: '우편번호', value: 'postcode' },
@@ -40,7 +44,7 @@ export const schemaList: {
   { name: '상세주소', value: 'addressDetail' },
   { name: '도시', value: 'city' },
   { name: '주/지역', value: 'region' },
-  { name: '이름 (username)', value: 'username' },
-  { name: '이름 (first name)', value: 'firstName' },
-  { name: '성 (last name)', value: 'lastName' },
+  { name: '이름_(username)', value: 'username' },
+  { name: '이름_(first_name)', value: 'firstName' },
+  { name: '성_(last_name)', value: 'lastName' },
 ]
--- packages/cli-plugins/gen-yup/src/utils/schema/purpose/Authentication.ts
+++ packages/cli-plugins/gen-yup/src/utils/schema/purpose/Authentication.ts
@@ -8,45 +8,45 @@ export const AuthenticationSchema = {
       requiredOrNot,
       `
         .test(
-        HELPER_TEXT['ID'].SPECIAL_CHARACTER,
-        HELPER_TEXT['ID'].SPECIAL_CHARACTER,
+        HELPER_TEXT['ID_SPECIAL_CHARACTER'],
+        HELPER_TEXT['ID_SPECIAL_CHARACTER'],
         (value) => !REGEX['SPECIAL_CHARACTER'].test(value || ''),
         )
-        .matches(REGEX['ID'], HELPER_TEXT['ID'].COMMON)`,
+        .matches(REGEX['ID'], HELPER_TEXT['ID_COMMON'])`,
     ),
 
   email: (requiredOrNot: RequiredOrNotText) =>
     getStringSchema(
       'email',
       requiredOrNot,
-      `.matches(REGEX['EMAIL'], HELPER_TEXT['EMAIL'])`,
+      `.matches(REGEX['EMAIL'], HELPER_TEXT['EMAIL_COMMON'])`,
     ),
 
   password: (requiredOrNot: RequiredOrNotText) =>
     getStringSchema(
       'password',
       requiredOrNot,
-      `.matches(REGEX['PASSWORD'], HELPER_TEXT['PASSWORD'])`,
+      `.matches(REGEX['PASSWORD'], HELPER_TEXT['PASSWORD_COMMON'])`,
     ),
 
   passwordConfirm: (requiredOrNot: RequiredOrNotText) =>
     getStringSchema(
       'passwordConfirm',
       requiredOrNot,
-      `.oneOf([yup.ref('password')], HELPER_TEXT['PASSWORD_CONFIRM'])`,
+      `.oneOf([yup.ref('password')], HELPER_TEXT['PASSWORD_NOT_EQ'])`,
     ),
 
   newPassword: (requiredOrNot: RequiredOrNotText) =>
     getStringSchema(
       'newPassword',
       requiredOrNot,
-      `.matches(REGEX['PASSWORD'], HELPER_TEXT['PASSWORD'])`,
+      `.matches(REGEX['PASSWORD'], HELPER_TEXT['PASSWORD_COMMON'])`,
     ),
 
   newPasswordConfirm: (requiredOrNot: RequiredOrNotText) =>
     getStringSchema(
       'newPasswordConfirm',
       requiredOrNot,
-      `.oneOf([yup.ref('newPassword')], HELPER_TEXT['PASSWORD_CONFIRM'])`,
+      `.oneOf([yup.ref('newPassword')], HELPER_TEXT['PASSWORD_NOT_EQ'])`,
     ),
 }
