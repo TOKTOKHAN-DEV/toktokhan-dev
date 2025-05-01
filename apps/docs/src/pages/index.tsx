605878be21b307b83b7e6fd2fd14ebed0ca5893d
pqr4579 <dlwjdwls6504@gmail.com>
2025-05-01 16:00:13 +0900

    feat(Section9.tsx): 🎸add Section9 & add purple-color

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -3,6 +3,8 @@ import HomeFooter from '../components/HomeFooter/HomeFooter'
 import { Section1 } from '../components/Section1'
 import { Section2 } from '../components/Section2'
 import { Section3 } from '../components/Section3'
+import { Section4 } from '../components/Section4'
+import { Section5 } from '../components/Section5'
 import { Section7 } from '../components/Section7'
 import { Section8 } from '../components/Section8'
 import { Section9 } from '../components/Section9'
@@ -14,6 +16,8 @@ export default function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
+      <Section5 />
       <Section7 />
       <Section8 />
       <Section9 />
