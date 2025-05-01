d0bdb0b43761201baf336a85c4b02989b76ec66f
keemtj <keemgreat@gmail.com>
2025-05-01 23:55:07 +0900

    feat: 🎸Add svg section3

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -2,6 +2,9 @@ import CommonLayout from '../components/@Layout/CommonLayout'
 import HomeFooter from '../components/HomeFooter/HomeFooter'
 import { Section1 } from '../components/Section1'
 import { Section2 } from '../components/Section2'
+import { Section3 } from '../components/Section3'
+import { Section4 } from '../components/Section4'
+import { Section5 } from '../components/Section5'
 import { Section10 } from '../components/Section10'
 
 export default function Home() {
@@ -9,6 +12,9 @@ export default function Home() {
     <CommonLayout header={null} footer={<HomeFooter />}>
       <Section1 />
       <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
       <Section10 />
     </CommonLayout>
   )
