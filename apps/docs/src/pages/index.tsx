9649a819066b643c0a7d69802695c995b02605a6
pqr4579 <dlwjdwls6504@gmail.com>
2025-05-03 10:50:39 +0900

    feat(Section8.tsx): 🎸add section8

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -4,7 +4,8 @@ import { Section1 } from '../components/Section1'
 import { Section2 } from '../components/Section2'
 import { Section3 } from '../components/Section3'
 import { Section4 } from '../components/Section4'
-import { Section6 } from '../components/Section6'
+import { Section5 } from '../components/Section5'
+import { Section8 } from '../components/Section8'
 import { Section9 } from '../components/Section9'
 import { Section10 } from '../components/Section10'
 
@@ -15,7 +16,8 @@ export default function Home() {
       <Section2 />
       <Section3 />
       <Section4 />
-      <Section6 />
+      <Section5 />
+      <Section8 />
       <Section9 />
       <Section10 />
     </CommonLayout>
