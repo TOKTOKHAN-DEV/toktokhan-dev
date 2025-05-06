0120043b7acdb708c61a6666968bc3581d794f9b
keemtj <keemgreat@gmail.com>
2025-05-07 01:35:04 +0900

    feat: 🎸Add section4

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -4,7 +4,6 @@ import { Section1 } from '../components/Section1'
 import { Section2 } from '../components/Section2'
 import { Section3 } from '../components/Section3'
 import { Section4 } from '../components/Section4'
-import { Section5 } from '../components/Section5'
 import { Section6 } from '../components/Section6'
 import { Section9 } from '../components/Section9'
 import { Section10 } from '../components/Section10'
@@ -16,7 +15,6 @@ export default function Home() {
       <Section2 />
       <Section3 />
       <Section4 />
-      <Section5 />
       <Section6 />
       <Section9 />
       <Section10 />
