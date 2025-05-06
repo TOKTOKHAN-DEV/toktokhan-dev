ac794a592bc47bccda327286d295eb88da0c909a
crabee <dlwjdwls6504@gmail.com>
2025-05-07 01:32:08 +0900

    feat(.changeset): 🎸section 반응형 및 interview svg 파일 삭제

--- apps/docs/src/components/Section7.tsx
+++ apps/docs/src/components/Section7.tsx
@@ -16,21 +16,40 @@ import {
 
 export const Section7 = () => {
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full mt-[240px]">
       <div className="mt-[64px] flex max-w-[100vw]">
-        <Marquee speed={150} autoFill pauseOnHover loop={0}>
-          <Client1Icon className="ml-[80px]" />
-          <Client2Icon className="ml-[80px]" />
-          <Client3Icon className="ml-[80px]" />
-          <Client4Icon className="ml-[80px]" />
-          <Client5Icon className="ml-[80px]" />
-          <Client6Icon className="ml-[80px]" />
-          <Client7Icon className="ml-[80px]" />
-          <Client8Icon className="ml-[80px]" />
-          <Client9Icon className="ml-[80px]" />
-          <Client10Icon className="ml-[80px]" />
-          <Client11Icon className="ml-[80px]" />
-        </Marquee>
+        <div className="md:hidden w-full max-w-[100vw]">
+          <Marquee speed={100} loop={0} autoFill>
+            <Client1Icon className="ml-[80px]" />
+            <Client2Icon className="ml-[80px]" />
+            <Client3Icon className="ml-[80px]" />
+            <Client4Icon className="ml-[80px]" />
+            <Client5Icon className="ml-[80px]" />
+            <Client6Icon className="ml-[80px]" />
+          </Marquee>
+          <Marquee speed={100} loop={0} autoFill direction="right">
+            <Client7Icon className="ml-[80px]" />
+            <Client8Icon className="ml-[80px]" />
+            <Client9Icon className="ml-[80px]" />
+            <Client10Icon className="ml-[80px]" />
+            <Client11Icon className="ml-[80px]" />
+          </Marquee>
+        </div>
+        <div className="base:hidden md:block full max-w-[100vw]">
+          <Marquee speed={150} autoFill pauseOnHover loop={0}>
+            <Client1Icon className="ml-[80px]" />
+            <Client2Icon className="ml-[80px]" />
+            <Client3Icon className="ml-[80px]" />
+            <Client4Icon className="ml-[80px]" />
+            <Client5Icon className="ml-[80px]" />
+            <Client6Icon className="ml-[80px]" />
+            <Client7Icon className="ml-[80px]" />
+            <Client8Icon className="ml-[80px]" />
+            <Client9Icon className="ml-[80px]" />
+            <Client10Icon className="ml-[80px]" />
+            <Client11Icon className="ml-[80px]" />
+          </Marquee>
+        </div>
       </div>
     </div>
   )
