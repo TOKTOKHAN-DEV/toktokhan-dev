7aac84df81a4b203993b35272084242d1b782e03
crabee <dlwjdwls6504@gmail.com>
2025-05-06 20:39:28 +0900

    feat(Section7.tsx): 🎸add client list section

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -3,8 +3,7 @@ import { HomeFooter } from '../components/HomeFooter/HomeFooter'
 import { Section1 } from '../components/Section1'
 import { Section2 } from '../components/Section2'
 import { Section3 } from '../components/Section3'
-import { Section4 } from '../components/Section4'
-import { Section5 } from '../components/Section5'
+import { Section7 } from '../components/Section7'
 import { Section8 } from '../components/Section8'
 import { Section9 } from '../components/Section9'
 import { Section10 } from '../components/Section10'
@@ -15,8 +14,7 @@ export default function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
-      <Section4 />
-      <Section5 />
+      <Section7 />
       <Section8 />
       <Section9 />
       <Section10 />
