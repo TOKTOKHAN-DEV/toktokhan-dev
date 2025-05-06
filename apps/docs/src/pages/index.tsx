78eb65ef8ccc36865a33f6d6b8118f7eb60e8949
crabee <dlwjdwls6504@gmail.com>
2025-05-06 21:18:14 +0900

    resolve conflict

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -5,6 +5,7 @@ import { Section2 } from '../components/Section2'
 import { Section3 } from '../components/Section3'
 import { Section4 } from '../components/Section4'
 import { Section5 } from '../components/Section5'
+import { Section6 } from '../components/Section6'
 import { Section7 } from '../components/Section7'
 import { Section8 } from '../components/Section8'
 import { Section9 } from '../components/Section9'
@@ -18,6 +19,7 @@ export default function Home() {
       <Section3 />
       <Section4 />
       <Section5 />
+      <Section6 />
       <Section7 />
       <Section8 />
       <Section9 />
